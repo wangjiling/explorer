744c58bc3655d45025992b0ad4979b21d48959fd
Bernardo Codesido <bernardo@rsk.co>
2016-09-12 16:06:27 -0300

    fix env variable

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ var filters = require('./filters')
 //var memcached = new Memcached("localhost:11211");
 
 module.exports = function(app){
-//  if (app.get('env') === 'development') 
-//    var web3relay = require('./web3dummy');
-//  else
+  if (app.get('env') === 'development') 
+    var web3relay = require('./web3dummy');
+  else
     var web3relay = require('./web3relay');
 
   var DAO = require('./dao');
